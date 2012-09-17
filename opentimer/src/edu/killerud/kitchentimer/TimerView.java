e91cea59a0ec1ca63a059e6abb5fbc781e45e73f
= <klerud@gmail.com>
2012-09-17 21:48:08 +0200

    OpenTimer v 2.1

--- opentimer/src/edu/killerud/kitchentimer/TimerView.java
+++ opentimer/src/edu/killerud/kitchentimer/TimerView.java
@@ -32,34 +32,34 @@ import android.widget.Toast;
 
 public class TimerView
 {
-	private final LinearLayout mLlTimerLayout;
+	private final LinearLayout contentLayout;
 
-	private int mTextSize;
+	private int textSize;
 
-	private TextView mTvHours;
-	private TextView mTvMinutes;
-	private TextView mTvSeconds;
-	private TextView mTvHourMinuteSeparator;
-	private TextView mTvMinuteSecondSeparator;
+	private TextView hours;
+	private TextView minutes;
+	private TextView seconds;
+	private TextView hourMinuteSeparator;
+	private TextView minuteSecondSeparator;
 
-	private static Context mContext;
-	private int mTimerViewId;
+	private static Context context;
+	private int timerViewId;
 
 	boolean isCounting;
 	boolean isSounding;
-	private CountdownService mCDService;
+	private CountdownService countdownService;
 
 	public TimerView(Context context, int timerViewId,
 			CountdownService serviceBinder)
 	{
-		mTextSize = 50;
-		mContext = context;
-		mCDService = serviceBinder;
-		mTimerViewId = timerViewId;
+		textSize = 50;
+		TimerView.context = context;
+		countdownService = serviceBinder;
+		this.timerViewId = timerViewId;
 
-		mLlTimerLayout = new LinearLayout(mContext);
-		mLlTimerLayout.setClickable(true);
-		mLlTimerLayout.setGravity(android.view.Gravity.CENTER);
+		contentLayout = new LinearLayout(TimerView.context);
+		contentLayout.setClickable(true);
+		contentLayout.setGravity(android.view.Gravity.CENTER);
 
 		setupLayouts();
 
@@ -67,155 +67,149 @@ public class TimerView
 		 * The shortClickListener. Here we start the timer and set the alarm, as
 		 * well as stop a sounding alarm.
 		 */
-		mLlTimerLayout.setOnClickListener(new OnClickListener()
-		{
+		contentLayout.setOnClickListener(new OnClickListener()
+        {
 
-			public synchronized void onClick(View v)
-			{
-				/*
-				 * We shut down the service to conserve resources when all
-				 * countdowns are finished (even if there was just one). In
-				 * order to be able to use the app right away, we need to start
-				 * the service on click.
-				 */
-				Intent startService = new Intent(mContext,
-						CountdownService.class);
-				mContext.startService(startService);
+            public synchronized void onClick(View v)
+            {
+                /*
+                     * We shut down the service to conserve resources when all
+                     * countdowns are finished (even if there was just one). In
+                     * order to be able to use the app right away, we need to start
+                     * the service on click.
+                     */
+                Intent startService = new Intent(TimerView.context,
+                        CountdownService.class);
+                TimerView.context.startService(startService);
 
-				if (isCounting)
-				{
-					Toast.makeText(mContext, R.string.how_to_stop,
-							Toast.LENGTH_SHORT).show();
-				} else if (isSounding)
-				{
-					/*
-					 * Here we release the wake lock we acquired further down in
-					 * the code, in KitchenCountDownTimer.onFinish(). We also
-					 * stop the annoying alarm. Then we reset the UI.
-					 */
+                if (isCounting)
+                {
+                    Toast.makeText(TimerView.context, R.string.how_to_stop,
+                            Toast.LENGTH_SHORT).show();
+                } else if (isSounding)
+                {
+                    /*
+                          * Here we release the wake lock we acquired further down in
+                          * the code, in KitchenCountDownTimer.onFinish(). We also
+                          * stop the annoying alarm. Then we reset the UI.
+                          */
 
-					// Intent stopAlarm = new Intent(mContext,
-					// edu.killerud.kitchentimer.CountdownService.class);
-					// stopAlarm.setAction("STOP_ALARM");
-					// stopAlarm.putExtra("TIMER_ID", mTimerViewId);
-					// mContext.startService(stopAlarm);
+                    countdownService.stopAlarm(TimerView.this.timerViewId);
 
-					mCDService.stopAlarm(mTimerViewId);
+                    hours.setTextColor(TimerView.context.getResources().getColor(
+                            R.color.white));
+                    minutes.setTextColor(TimerView.context.getResources().getColor(
+                            R.color.white));
+                    seconds.setTextColor(TimerView.context.getResources().getColor(
+                            R.color.white));
+                    hourMinuteSeparator.setTextColor(TimerView.context.getResources()
+                            .getColor(R.color.white));
+                    minuteSecondSeparator.setTextColor(TimerView.context
+                            .getResources().getColor(R.color.white));
+                    isSounding = false;
+                } else if (!isCounting && !isSounding)
+                {
 
-					mTvHours.setTextColor(mContext.getResources().getColor(
-							R.color.white));
-					mTvMinutes.setTextColor(mContext.getResources().getColor(
-							R.color.white));
-					mTvSeconds.setTextColor(mContext.getResources().getColor(
-							R.color.white));
-					mTvHourMinuteSeparator.setTextColor(mContext.getResources()
-							.getColor(R.color.white));
-					mTvMinuteSecondSeparator.setTextColor(mContext
-							.getResources().getColor(R.color.white));
-					isSounding = false;
-				} else if (!isCounting && !isSounding)
-				{
+                    /* Set a new alarm and start counting down! */
+                    Integer hours = OpenTimerActivity.getHours();
+                    Integer minutes = OpenTimerActivity.getMinutes();
+                    Integer seconds = OpenTimerActivity.getSeconds();
 
-					/* Set a new alarm and start counting down! */
-					Integer hours = OpenTimerActivity.getHours();
-					Integer minutes = OpenTimerActivity.getMinutes();
-					Integer seconds = OpenTimerActivity.getSeconds();
+                    Long millisInFuture = (long) ((seconds * 1000)
+                            + (minutes * 60 * 1000) + (hours * 60 * 60 * 1000));
 
-					Long millisInFuture = (long) ((seconds * 1000)
-							+ (minutes * 60 * 1000) + (hours * 60 * 60 * 1000));
+                    if (millisInFuture < 1000)
+                    {
+                        Toast.makeText(TimerView.context, R.string.enter_higher,
+                                Toast.LENGTH_SHORT).show();
+                        return;
+                    }
 
-					if (millisInFuture < 1000)
-					{
-						Toast.makeText(mContext, R.string.enter_higher,
-								Toast.LENGTH_SHORT).show();
-						return;
-					}
+                    TimerView.this.hours.setTextColor(TimerView.context.getResources().getColor(
+                            R.color.white));
+                    TimerView.this.minutes.setTextColor(TimerView.context.getResources().getColor(
+                            R.color.white));
+                    TimerView.this.seconds.setTextColor(TimerView.context.getResources().getColor(
+                            R.color.white));
+                    hourMinuteSeparator.setTextColor(TimerView.context.getResources()
+                            .getColor(R.color.white));
+                    minuteSecondSeparator.setTextColor(TimerView.context
+                            .getResources().getColor(R.color.white));
 
-					mTvHours.setTextColor(mContext.getResources().getColor(
-							R.color.white));
-					mTvMinutes.setTextColor(mContext.getResources().getColor(
-							R.color.white));
-					mTvSeconds.setTextColor(mContext.getResources().getColor(
-							R.color.white));
-					mTvHourMinuteSeparator.setTextColor(mContext.getResources()
-							.getColor(R.color.white));
-					mTvMinuteSecondSeparator.setTextColor(mContext
-							.getResources().getColor(R.color.white));
+                    countdownService.startTimer(TimerView.this.timerViewId, millisInFuture);
+                    isCounting = true;
+                }
 
-					mCDService.startTimer(mTimerViewId, millisInFuture);
-					isCounting = true;
-				}
+            }
 
-			}
-
-		});
+        });
 
 		/*
 		 * The longClickListener. Here we stop a countdown and alarm while it is
 		 * in progress (hasn't sounded yet).
 		 */
-		mLlTimerLayout
+		contentLayout
 				.setOnLongClickListener(new android.view.View.OnLongClickListener()
-				{
-					public synchronized boolean onLongClick(View v)
-					{
-						if (isCounting)
-						{
-							/* Reset the UI */
-							resetUI();
-							mCDService.stopTimer(mTimerViewId);
-							isCounting = false;
-						}
-						return true;
-					}
-				});
+                {
+                    public synchronized boolean onLongClick(View v)
+                    {
+                        if (isCounting)
+                        {
+                            /* Reset the UI */
+                            resetUI();
+                            countdownService.stopTimer(TimerView.this.timerViewId);
+                            isCounting = false;
+                        }
+                        return true;
+                    }
+                });
 	}
 
 	/* Sets up the Timer UI */
 	protected void setupLayouts()
 	{
 
-		mTvHours = new TextView(mLlTimerLayout.getContext());
-		mTvMinutes = new TextView(mLlTimerLayout.getContext());
-		mTvSeconds = new TextView(mLlTimerLayout.getContext());
-		mTvHourMinuteSeparator = new TextView(mLlTimerLayout.getContext());
-		mTvMinuteSecondSeparator = new TextView(mLlTimerLayout.getContext());
+		hours = new TextView(contentLayout.getContext());
+		minutes = new TextView(contentLayout.getContext());
+		seconds = new TextView(contentLayout.getContext());
+		hourMinuteSeparator = new TextView(contentLayout.getContext());
+		minuteSecondSeparator = new TextView(contentLayout.getContext());
 
-		mTvHours.setTextSize(mTextSize);
-		mTvMinutes.setTextSize(mTextSize);
-		mTvSeconds.setTextSize(mTextSize);
-		mTvHourMinuteSeparator.setTextSize(mTextSize);
-		mTvMinuteSecondSeparator.setTextSize(mTextSize);
+		hours.setTextSize(textSize);
+		minutes.setTextSize(textSize);
+		seconds.setTextSize(textSize);
+		hourMinuteSeparator.setTextSize(textSize);
+		minuteSecondSeparator.setTextSize(textSize);
 
 		resetUI();
 
-		mLlTimerLayout.addView(mTvHours);
-		mLlTimerLayout.addView(mTvHourMinuteSeparator);
-		mLlTimerLayout.addView(mTvMinutes);
-		mLlTimerLayout.addView(mTvMinuteSecondSeparator);
-		mLlTimerLayout.addView(mTvSeconds);
+		contentLayout.addView(hours);
+		contentLayout.addView(hourMinuteSeparator);
+		contentLayout.addView(minutes);
+		contentLayout.addView(minuteSecondSeparator);
+		contentLayout.addView(seconds);
 
-		mLlTimerLayout
+		contentLayout
 				.setBackgroundResource(R.drawable.status_border_grey_slim);
 	}
 
 	protected void resetUI()
 	{
-		mTvHours.setTextColor(mContext.getResources().getColor(R.color.white));
-		mTvMinutes
-				.setTextColor(mContext.getResources().getColor(R.color.white));
-		mTvSeconds
-				.setTextColor(mContext.getResources().getColor(R.color.white));
-		mTvHourMinuteSeparator.setTextColor(mContext.getResources().getColor(
-				R.color.white));
-		mTvMinuteSecondSeparator.setTextColor(mContext.getResources().getColor(
-				R.color.white));
+		hours.setTextColor(context.getResources().getColor(R.color.white));
+		minutes
+				.setTextColor(context.getResources().getColor(R.color.white));
+		seconds
+				.setTextColor(context.getResources().getColor(R.color.white));
+		hourMinuteSeparator.setTextColor(context.getResources().getColor(
+                R.color.white));
+		minuteSecondSeparator.setTextColor(context.getResources().getColor(
+                R.color.white));
 
-		mTvHourMinuteSeparator.setText(":");
-		mTvMinuteSecondSeparator.setText(":");
-		mTvHours.setText("00");
-		mTvMinutes.setText("00");
-		mTvSeconds.setText("00");
+		hourMinuteSeparator.setText(":");
+		minuteSecondSeparator.setText(":");
+		hours.setText("00");
+		minutes.setText("00");
+		seconds.setText("00");
 	}
 
 	public void updateTick(long millisUntillFinished)
@@ -224,15 +218,15 @@ public class TimerView
 		int minutes = (int) (millisUntillFinished / 60000) - (hours * 60);
 		int seconds = (int) (millisUntillFinished / 1000) - (hours * 60 * 60)
 				- (minutes * 60);
-		mTvHours.setText((hours < 10) ? "0" + hours : "" + hours);
-		mTvMinutes.setText((minutes < 10) ? "0" + minutes : "" + minutes);
-		mTvSeconds.setText((seconds < 10) ? "0" + seconds : "" + seconds);
+		this.hours.setText((hours < 10) ? "0" + hours : "" + hours);
+		this.minutes.setText((minutes < 10) ? "0" + minutes : "" + minutes);
+		this.seconds.setText((seconds < 10) ? "0" + seconds : "" + seconds);
 	}
 
 	/* Used by the UI for object reference */
 	public LinearLayout getTimerLayout()
 	{
-		return mLlTimerLayout;
+		return contentLayout;
 	}
 
 	/*
@@ -241,9 +235,9 @@ public class TimerView
 	 */
 	public void remove()
 	{
-		if (mLlTimerLayout != null)
+		if (contentLayout != null)
 		{
-			mLlTimerLayout.removeAllViews();
+			contentLayout.removeAllViews();
 		}
 	}
 
@@ -257,19 +251,19 @@ public class TimerView
 	{
 		isSounding = true;
 		isCounting = false;
-		mTvHours.setTextColor(mContext.getResources().getColor(R.color.red));
-		mTvMinutes.setTextColor(mContext.getResources().getColor(R.color.red));
-		mTvSeconds.setTextColor(mContext.getResources().getColor(R.color.red));
-		mTvHourMinuteSeparator.setTextColor(mContext.getResources().getColor(
-				R.color.red));
-		mTvMinuteSecondSeparator.setTextColor(mContext.getResources().getColor(
-				R.color.red));
+		hours.setTextColor(context.getResources().getColor(R.color.red));
+		minutes.setTextColor(context.getResources().getColor(R.color.red));
+		seconds.setTextColor(context.getResources().getColor(R.color.red));
+		hourMinuteSeparator.setTextColor(context.getResources().getColor(
+                R.color.red));
+		minuteSecondSeparator.setTextColor(context.getResources().getColor(
+                R.color.red));
 
-		mTvHourMinuteSeparator.setText(":");
-		mTvMinuteSecondSeparator.setText(":");
-		mTvHours.setText("00");
-		mTvMinutes.setText("00");
-		mTvSeconds.setText("00");
+		hourMinuteSeparator.setText(":");
+		minuteSecondSeparator.setText(":");
+		hours.setText("00");
+		minutes.setText("00");
+		seconds.setText("00");
 
 	}
 
